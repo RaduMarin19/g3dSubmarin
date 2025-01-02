ce5ad6f822b468fda55a76ea70d3cb79b958b21c
RaduMarin19 <marinradul33t@gmail.com>
2025-01-02 21:59:50 +0200

    Revert "includes"
    This reverts commit a7031c10ae6a969db024efd6359dc90e27f58281.

--- g3dSubmarin/g3dSubmarin/includes.h
+++ g3dSubmarin/g3dSubmarin/includes.h
@@ -5,29 +5,31 @@
 #ifndef INCLUDES_H
 #define INCLUDES_H
 
+
 #include <iostream>
-#include <cstdlib>
-#include <cstdio>
+#include <stdlib.h>
+#include <stdio.h>
 #include <vector>
 #include <fstream>
 #include <string>
-#include <cmath>
-#include <memory>
+#include <math.h>
+#include <memory.h>
+#include <fstream>
 #include <sstream>
 #include <random>
 
-#include <GL/glew.h>
-#include <GL/freeglut.h>
-#include <GLFW/glfw3.h>
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "GLFW/glfw3.h"
 
-#ifdef __linux__
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#else
+#if defined linux
 #include "glm/glm.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#else
+#include "GLM.hpp"
+#include "gtc/type_ptr.hpp"
+#include "gtc/matrix_transform.hpp"
 #pragma comment (lib, "glfw3dll.lib")
 #pragma comment (lib, "glew32.lib")
 #pragma comment (lib, "OpenGL32.lib")
